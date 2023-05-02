4c027c60ae980c7576d240f2bd6367d26df93397
Codiku <111140836+codiku-dev@users.noreply.github.com>
2023-05-02 18:19:28 +0200

    Add files via upload

---
+++ App.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  square: {
+    backgroundColor: "orange",
+    height: 100,
+    width: 100,
+  },
+});
+
+export { s };
