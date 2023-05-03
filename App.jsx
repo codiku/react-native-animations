a1f5986bfc79f3200ab8ce69b7fd2605ca1f3a18
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-03 18:15:02 +0200

    wip

--- App.jsx
+++ App.jsx
@@ -1,32 +1,76 @@
-import { LogBox, View } from "react-native";
-import { s } from "./App.style";
+import { StyleSheet, View } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
 import Animated, {
+  useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
-  withTiming,
+  withSpring,
 } from "react-native-reanimated";
-import { useEffect } from "react";
-LogBox.ignoreLogs(["No native splash"]);
+const SQUARE_SIZE = 100;
+const CIRCLE_PERIMETER = 400;
+const CIRCLE_RADIUS = CIRCLE_PERIMETER / 2;
 
 export default function App() {
-  // 1 - Declare an animation value
-  const squareAnimX = useSharedValue(0);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
-  // 2 - Run animation and update the animation value
-  useEffect(() => {
-    squareAnimX.value = withTiming(300, { duration: 2000 });
-  }, []);
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.initialX = translateX.value;
+      ctx.initialY = translateY.value;
+    },
+    onActive: (e, ctx) => {
+      translateX.value = ctx.initialX + e.translationX;
+      translateY.value = ctx.initialY + e.translationY;
+      // translateY.value = event.translationY;
+    },
+  });
 
-  // 3 - Create an animated style and send it the animation value
-  const squareAnimStyle = useAnimatedStyle(() => {
+  const animatedStyle = useAnimatedStyle(() => {
     return {
-      left: squareAnimX.value,
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
     };
   });
-  // 4 - Send the animated style to an animated component
   return (
-    <View style={s.root}>
-      <Animated.View style={[s.square, squareAnimStyle]} />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[s.square, animatedStyle]} />
+        </PanGestureHandler>
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  square: {
+    height: SQUARE_SIZE,
+    width: SQUARE_SIZE,
+    backgroundColor: "rgba(0,140,256,1)",
+    borderRadius: 15,
+  },
+  circle: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: CIRCLE_PERIMETER,
+    width: CIRCLE_PERIMETER,
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: "rgba(0,140,256,1)",
+  },
+});
