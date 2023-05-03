2efdc6d61b90228f93aec968daa2d6a0d53c90d9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-03 15:32:29 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { LogBox, View } from "react-native";
 import { s } from "./App.style";
 import Animated, {
   useAnimatedStyle,
@@ -6,23 +6,24 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 import { useEffect } from "react";
+LogBox.ignoreLogs(["No native splash"]);
 
 export default function App() {
-  // 1 - Define an animation value
+  // 1 - Declare an animation value
   const squareAnimX = useSharedValue(0);
 
-  // 2 - Run the animation and update the anim value
+  // 2 - Run animation and update the animation value
   useEffect(() => {
     squareAnimX.value = withTiming(300, { duration: 2000 });
   }, []);
 
-  // 3 - Create a style that will contain the animation value
+  // 3 - Create an animated style and send it the animation value
   const squareAnimStyle = useAnimatedStyle(() => {
     return {
       left: squareAnimX.value,
     };
   });
-  // 4 - Send the style to an Animated component
+  // 4 - Send the animated style to an animated component
   return (
     <View style={s.root}>
       <Animated.View style={[s.square, squareAnimStyle]} />
