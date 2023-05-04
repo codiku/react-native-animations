ce45e2553855b2431f1adfae615318a8e60be6a0
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-04 12:30:41 +0200

    wip

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { LogBox, View } from "react-native";
 import {
   GestureHandlerRootView,
   PanGestureHandler,
@@ -11,37 +11,41 @@ import Animated, {
 } from "react-native-reanimated";
 import { s, CIRCLE_RADIUS } from "./App.style";
 
+LogBox.ignoreLogs(["No native splash"]);
+
 export default function App() {
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
+  const squareAnimTranslateX = useSharedValue(0);
+  const squareAnimTranslateY = useSharedValue(0);
 
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, ctx) => {
-      ctx.initialPosX = translateX.value;
-      ctx.initialPosY = translateY.value;
+      ctx.initialPosX = squareAnimTranslateX.value;
+      ctx.initialPosY = squareAnimTranslateY.value;
     },
-    onActive: (event, ctx) => {
-      translateX.value = ctx.initialPosX + event.translationX;
-      translateY.value = ctx.initialPosY + event.translationY;
+    onActive: (e, ctx) => {
+      squareAnimTranslateX.value = ctx.initialPosX + e.translationX;
+      squareAnimTranslateY.value = ctx.initialPosY + e.translationY;
     },
-    onEnd: (_) => {
-      const distance = Math.sqrt(translateX.value ** 2 + translateY.value ** 2);
-      console.log("***", "end", distance, CIRCLE_RADIUS);
-      if (distance < CIRCLE_RADIUS) {
-        translateX.value = withSpring(0);
-        translateY.value = withSpring(0);
+    onEnd: (e, ctx) => {
+      console.log("onEnd");
+      const distanceFromCenter = Math.sqrt(
+        squareAnimTranslateX.value ** 2 + squareAnimTranslateY.value ** 2
+      );
+      if (distanceFromCenter < CIRCLE_RADIUS) {
+        squareAnimTranslateX.value = withSpring(0);
+        squareAnimTranslateY.value = withSpring(0);
       }
     },
   });
 
-  const animatedStyle = useAnimatedStyle(() => {
+  const squareAnimStyle = useAnimatedStyle(() => {
     return {
       transform: [
         {
-          translateX: translateX.value,
+          translateX: squareAnimTranslateX.value,
         },
         {
-          translateY: translateY.value,
+          translateY: squareAnimTranslateY.value,
         },
       ],
     };
@@ -51,7 +55,7 @@ export default function App() {
       <View style={s.root}>
         <View style={s.circle}>
           <PanGestureHandler onGestureEvent={gestureHandler}>
-            <Animated.View style={[s.square, animatedStyle]} />
+            <Animated.View style={[s.square, squareAnimStyle]} />
           </PanGestureHandler>
         </View>
       </View>
--- App.style.js
+++ App.style.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 export const SQUARE_SIZE = 100;
-export const CIRCLE_PERIMETER = 400;
+export const CIRCLE_PERIMETER = 350;
 export const CIRCLE_RADIUS = CIRCLE_PERIMETER / 2;
 export const s = StyleSheet.create({
   root: {
@@ -9,18 +9,18 @@ export const s = StyleSheet.create({
     alignItems: "center",
   },
   square: {
+    borderRadius: 15,
+    backgroundColor: "orange",
     height: SQUARE_SIZE,
     width: SQUARE_SIZE,
-    backgroundColor: "rgba(0,140,256,1)",
-    borderRadius: 15,
   },
   circle: {
-    justifyContent: "center",
-    alignItems: "center",
+    borderWidth: 5,
+    borderColor: "orange",
     height: CIRCLE_PERIMETER,
     width: CIRCLE_PERIMETER,
     borderRadius: CIRCLE_RADIUS,
-    borderWidth: 5,
-    borderColor: "rgba(0,140,256,1)",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
