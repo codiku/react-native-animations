b8a7d9b4c65f7dd0b1684e929273a00825d139c4
Codiku <111140836+codiku-dev@users.noreply.github.com>
2023-05-05 19:27:03 +0200

    Update List.jsx

--- components/List/List.jsx
+++ components/List/List.jsx
@@ -2,11 +2,14 @@ import { ScrollView } from "react-native-gesture-handler";
 import { s } from "./List.style";
 import { IMAGES } from "../../constant";
 import { ListItem } from "../ListItem/ListItem";
+import { useSharedValue } from "react-native-reanimated"
+
 export function List() {
+  const yDistance =  useSharedValue(0)
   return (
     <ScrollView>
-      {IMAGES.map((image) => (
-        <ListItem image={image} />
+      {IMAGES.map((image,i) => (
+        <ListItem image={image} yDistance={yDistance} key={image.title+i}/>
       ))}
     </ScrollView>
   );
