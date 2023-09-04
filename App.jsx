d871f8e9accb6daf26b8538400b3b6ed099e6019
Codiku <111140836+codiku-dev@users.noreply.github.com>
2023-09-04 20:52:57 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { Dimensions, LogBox, View } from "react-native";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { s } from "./App.style";
 import { List } from "./components/List/List";
 LogBox.ignoreLogs(["No native splash"]);
@@ -7,8 +7,8 @@ LogBox.ignoreLogs(["No native splash"]);
 const { width: SCREEN_W, height: SCREEN_H } = Dimensions.get("screen");
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <List />
-    </View>
+    </GestureHandlerRootView>
   );
 }
